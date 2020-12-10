4f2a5e9617c6b38d91edbf0aea5a309b3526a772
manish877050 <75736259+manish877050@users.noreply.github.com>
2020-12-10 19:17:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 var express = require('express');
 var app = express();
-var path=require("path");
+//var path=require("path");
 var fs=require("fs");
 
 
 //server configuration
-app.use(express.static(path.join(__dirname,'public')));
+///app.use(express.static(path.join(__dirname,'public')));
 
- app.get('/', function (req, res) {
-   res.sendFile(path.join(___dirname + '/index.html'));
+ app.get('/',(requset, response) =>{
+
+    console.log('inside / get router')
+    response.send('inside / get router')
+
 });
  
 
-app.get('/hello', function (req, res) {
-  console.log("CAlling rest api");
-  var person={firstName:'manish',lastName:'prajapati',age:22};
+app.get('/hello',(req, res)=> {
+  console.log("Calling rest api");
+  var person={firstName:'Manish',lastName:'Prajapati',age:22};
   res.send(person);
 });
 
- 
-var server = app.listen(8081, function () {
 
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("Example app listening at http://localhost:8081", host, port)
+app.listen(8081 ,'0.0.0.0',()=>{
+    console.log("Example app listening at http://localhost:8081")
 })
